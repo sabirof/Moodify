53b810efd386e0ff637de95dbef5ca79ff878abf
sabirof <maharram.sabir@gmail.com>
2023-05-24 16:07:28 +0200

    firebase

--- moodify-api/src/context/AuthContext.jsx
+++ moodify-api/src/context/AuthContext.jsx
@@ -1,5 +1,7 @@
 import { createContext } from "react";
 import { useState } from "react";
+import { auth } from "../FireConfig/FireConfig";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 
 
@@ -9,6 +11,20 @@ export const AuthContext = createContext()
 
 
 export const AuthContextProvider = (props) => {
+    
+
+
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
 
     const [user, setUser] = useState(null);
 
