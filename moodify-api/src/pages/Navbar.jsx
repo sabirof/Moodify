367b37c72d005fa499c608c0209172a3c4579759
sabirof <maharram.sabir@gmail.com>
2023-05-26 14:05:26 +0200

    navbar align right

--- moodify-api/src/pages/Navbar.jsx
+++ moodify-api/src/pages/Navbar.jsx
@@ -19,21 +19,30 @@ function Navbar() {
           <Link to="/genres" className="nav-link">Genres</Link>
         </li>
         <li className="nav-item">
-          {user ? (
-            <>
-              <span className="nav-link" style={{ color: 'green' }}>Logged in as: {user.email}</span>
-              <button className="nav-link" onClick={handleLogout} style={{ color: 'red' }}>Logout</button>
-            </>
-          ) : (
-            <>
-              <Link to="/register" className="nav-link">Register</Link> | 
-              <Link to="/login" className="nav-link">Login</Link>
-            </>
-          )}
-        </li>
-        <li className="nav-item">
           <Link to="/about" className="nav-link">About</Link>
         </li>
+      </ul>
+      
+      <ul className="navbar-nav ml-auto"> 
+        {user ? (
+          <>
+            <li className="nav-item">
+              <span className="nav-link" style={{ color: 'green' }}>Logged in as: {user.email}</span>
+            </li>
+            <li className="nav-item">
+              <button className="nav-link" onClick={handleLogout} style={{ color: 'red' }}>Logout</button>
+            </li>
+          </>
+        ) : (
+          <>
+            <li className="nav-item">
+              <Link to="/register" className="nav-link">Register</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/login" className="nav-link">Login</Link>
+            </li>
+          </>
+        )}
       </ul>
     </div>
   );
